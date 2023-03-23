bde40422a8abc794389b23a448d4cc6b5dc5e1bf
NaLy's <buon0a2003@gmail.com>
2023-03-23 21:54:02 +0700

    add lai cai git ignore

---
+++ WebbyMyself/Models/StudentManageEntities.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebbyMyself.Models;
+
+namespace WebbyMyself.Models
+{
+    public class StudentManageEntities : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBulder)
+        {
+            optionsBulder.UseSqlServer(
+                "Data Source=DESKTOP-24T1NPF; Initial Catalog=SV;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+            );
+        }
+        public virtual DbSet<Branch> Branch { get; set; }
+        public virtual DbSet<Class> Class { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+    }
+}
