52734bf4271897e86da1fc5885cf943fcba72e5d
NaLy's <91066936+buon0a2003@users.noreply.github.com>
2023-03-27 20:28:12 +0700

    Add files via upload
    real sql test

---
+++ SQL-test.sql
@@ -0,0 +1,68 @@
+﻿USE master;
+DROP DATABASE IF EXISTS SV;
+GO
+CREATE DATABASE SV;
+GO
+USE SV;
+GO
+DROP TABLE IF EXISTS Branch
+GO
+CREATE TABLE Branch
+(
+	ID int primary key,
+	BranchName nvarchar(100)
+)
+DROP TABLE IF EXISTS Class
+GO
+CREATE TABLE Class
+(
+    ID int primary key,
+    ClassName nvarchar(50),
+	BranchID int foreign key references Branch(ID)
+)
+GO
+DROP TABLE IF EXISTS Student
+GO
+CREATE TABLE Student
+( 
+    ID int PRIMARY KEY,
+    [Name] nvarchar(50) NOT NULL,
+    Phone nvarchar(50) NOT NULL,
+    Email nvarchar(50) NOT NULL,
+    [Address] nvarchar(50),
+    ClassID int foreign key references Class(ID)
+)
+
+INSERT INTO Branch VALUES (1, N'Công nghệ thông tin')
+INSERT INTO Branch VALUES (2, N'Công nghệ phần mềm')
+INSERT INTO Branch VALUES (3, N'Truyền thông mạng máy tính')
+
+INSERT INTO Class VALUES (1, N'CNT62DH',1)
+INSERT INTO Class VALUES (2, N'KPM62DH',2)
+INSERT INTO Class VALUES (3, N'TTM62DH',3)
+
+INSERT INTO Student VALUES (90658, N'Nguyen Trung Son', N'0123456789', N'burhlmeo@gmail.com', N'HP', 1)
+INSERT INTO Student VALUES (10000, N'Tran Ba Quoc Dung', N'0987654321', N'lmeo@gmail.com', N'HP', 2)
+
+DROP VIEW IF EXISTS ClassManage
+GO
+CREATE VIEW ClassManage AS
+SELECT Class.ID, Class.ClassName, Branch.BranchName
+FROM Branch, Class
+where Class.BranchID = Branch.ID
+GO
+
+DROP VIEW IF EXISTS StudentManage 
+GO
+CREATE VIEW StudentManage AS
+SELECT Student.ID, Student.[Name], Phone, Email, [Address], ClassName
+FROM Student, Class
+WHERE Student.ClassID = Class.ID
+
+GO
+
+select * 
+from ClassManage
+
+select * 
+FROM StudentManage
